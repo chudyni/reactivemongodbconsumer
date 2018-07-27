15d9f6d79627ca0a5bb8233864aaf7a9e6b9a669
Marcin Bracisiewicz <Marcin.Bracisiewicz@performgroup.com>
2018-07-27 15:04:20 +0200

    Generated project

---
+++ src/main/java/com/home/reactivemongodbconsumer/ReactivemongodbconsumerApplication.java
@@ -0,0 +1,12 @@
+package com.home.reactivemongodbconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivemongodbconsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactivemongodbconsumerApplication.class, args);
+  }
+}
